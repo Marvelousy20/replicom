1a23bad8ac0e4cb8f2066efacbff5218d83a6fdf
GohYiHao <gohyihao0727@gmail.com>
2024-03-31 01:22:09 +0100

    making changes

--- src/app/api/output/route.ts
+++ src/app/api/output/route.ts
@@ -1,30 +1,70 @@
 import axios from "axios";
 
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
+import { type NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  console.log("Request received:", req.method, req.body);
+export async function POST(request: Request, res: NextApiResponse) {
+  const body = await request.json();
+  console.log(body);
+  try {
+    const response = await fetch("https://api.replicate.com/v1/predictions", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to make prediction" });
+  }
+}
 
-  if (req.method === "POST") {
-    try {
-      const response = await axios.post(
-        "https://api.replicate.com/v1/predictions",
-        req.body,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to make prediction" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    console.log("Request received:", req.method, req.body);
-    console.log("Method not allowed", req.method);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const predictionId = searchParams.get("id");
+  console.log(predictionId);
+  if (!predictionId) {
+    return new NextResponse(
+      JSON.stringify({ error: "Prediction ID parameter is required" }),
+      {
+        status: 400, // Bad Request
+        headers: {
+          Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  try {
+    // Make the request to the Replicate API for fetching the prediction result
+    const response = await axios.get(
+      `https://api.replicate.com/v1/predictions/${predictionId}`,
+      {
+        headers: {
+          Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Return the response data
+    return new NextResponse(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error);
+    // Return an error response
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to fetch prediction result" }),
+      {
+        status: 500, // Internal Server Error
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 }
--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -10,11 +10,17 @@ type DynamicFormProps = {
   version: string;
 };
 
+type OutputProps = {
+  id: string;
+  model: string;
+};
+
 const DynamicForm: React.FC<DynamicFormProps> = ({ schema, version }) => {
   const [formData, setFormData] = useState<FormData>({});
+  const [output, setOput] = useState({});
 
   const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement>,
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
     key: string
   ) => {
     const value =
@@ -26,17 +32,62 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ schema, version }) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(formData);
+
+    const inputData = Object.fromEntries(
+      Object.entries(formData).map(([key, value]) => {
+        const valueStr = typeof value === "number" ? value.toString() : value;
+
+        return [key, parseInt(valueStr, 10)];
+      })
+    );
+    const requestBody = { version, input: inputData };
+    console.log("Request Body:", JSON.stringify(requestBody, null, 2));
+
     const response = await fetch("/api/output", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(requestBody),
     });
     const data = await response.json();
-    console.log(data);
+    console.log("DATA", data);
+
+    const predictionId = data.id;
+    const predictionResult = await pollPrediction(predictionId);
+    setOput(predictionResult);
   };
+
+  async function pollPrediction(
+    predictionId: string,
+    interval = 5000,
+    maxAttempts = 10
+  ) {
+    let attempts = 0;
+    while (attempts < maxAttempts) {
+      const response = await fetch(`/api/output?id=${predictionId}`, {
+        headers: {
+          Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+
+      if (data.status === "completed") {
+        console.log("Prediction completed:", data);
+        return data;
+      } else if (data.status === "failed") {
+        console.error("Prediction failed:", data);
+        return null;
+      }
+
+      attempts++;
+      await new Promise((resolve) => setTimeout(resolve, interval));
+    }
+
+    console.error("Max attempts reached without completion.");
+    return null;
+  }
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
@@ -45,23 +96,27 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ schema, version }) => {
           <label htmlFor={key} className="text-sm font-medium text-gray-700">
             {key}
           </label>
-          {field.type === "file" ? (
-            <input
-              type="file"
+          {["top", "bottom", "right", "left"].includes(key) ? (
+            <select
               id={key}
               name={key}
+              value={formData[key] || ""}
               onChange={(event) => handleInputChange(event, key)}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
+              className="mt-1 px-2 py-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 outline-none"
+            >
+              <option value="0">0</option>
+              <option value="256">256</option>
+              <option value="512">516</option>
+              <option value="768">768</option>
+            </select>
           ) : (
             <input
-              type={field.type || "text"}
+              type={field.type || "number"}
               id={key}
               name={key}
               defaultValue={field.default?.toString() || ""}
               onChange={(event) => handleInputChange(event, key)}
               className="mt-1 px-2 py-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 outline-none"
-              //   />
             />
           )}
           <small className="mt-1">{field.description}</small>
